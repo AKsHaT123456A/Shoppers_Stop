9d548cad28f24a68512833a096820ee7e122ac3c
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-02-07 17:39:29 +0530

    E-commerce

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router=createBrowserRouter({
+  path:
+})
+function App() {
+  return (
+    <div>
+     <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
